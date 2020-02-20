3c72c2eb749f9de1b6e47b0cd13e6440c5ce25aa
satoshi0211wata <satoshi.2.11@gmail.com>
2020-02-20 12:21:14 +0900

    before trasition

---
+++ src/components/CountNumber.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <h2>{{ title | lowerCase | upperCase }}</h2>
+    <p>{{ subTitle | upperCase | lowerCase }}</p>
+    <P>{{ number }}</P>
+    <button @click="number++">+1</button>
+  </div>
+
+</template>
+
+<script>
+import { tokyoNumber } from "@/tokyoNumber";
+
+export default {
+  mixins: [tokyoNumber],
+  created() {
+    console.log("created in component");
+  }
+};
+</script>
--- src/main.js
+++ src/main.js
@@ -1,7 +1,17 @@
 import Vue from 'vue'
 import App from './App.vue'
+import LikeNumber from "./components/LikeNumber.vue"
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
+Vue.component('LikeNumber', LikeNumber);
+Vue.filter("upperCase", function(value){
+  return value.toUpperCase();
+});
+Vue.mixin({
+  created() {
+    console.log("grobalMixin");
+  }
+});
 
 new Vue({
   render: h => h(App),
